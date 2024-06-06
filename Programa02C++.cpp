d1a86dfc0eb2ac4b571d7f3790c83b9a4e118130
Luisa Lima <168607491+luilimon@users.noreply.github.com>
2024-06-06 11:20:45 -0600

    Update Programa02C++.cpp

--- Programa02C++.cpp
+++ Programa02C++.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int num1, num2;
 
-    std::cout << "Ingresa el primer numero: ";
-    std::cin >> num1;
-    std::cout << "Ingresa tu segundo numero: ";
-    std::cin >> num2;
+    cout << "Ingresa el primer numero: ";
+    cin >> num1;
+    cout << "Ingresa tu segundo numero: ";
+    cin >> num2;
 
     if (num1 == num2) {
-        std::cout << "Los numeros son iguales." << std::endl;
+        cout << "Los numeros son iguales." << std::endl;
     } else if (num1 > num2) {
-        std::cout << "El primer numero es mayor que el segundo." << std::endl;
+        cout << "El primer numero es mayor que el segundo." << std::endl;
     } else {
-        std::cout << "El segundo numero es mayor que el primero." << std::endl;
+        cout << "El segundo numero es mayor que el primero." << std::endl;
     }
 
     return 0;
