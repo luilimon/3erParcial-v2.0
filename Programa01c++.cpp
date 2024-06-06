b249862c3d100d5ea7952250db3800d68358f0fd
Luisa Lima <168607491+luilimon@users.noreply.github.com>
2024-06-06 11:19:02 -0600

    Update Programa01c++.cpp

--- Programa01c++.cpp
+++ Programa01c++.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-
+using namespace std;
 int main() 
 {
     int num1, num2, num3;
 
-    std::cout << "Dame el primer numero: ";
-    std::cin >> num1;
-    std::cout << "Dame el segundo numero: ";
-    std::cin >> num2;
-    std::cout << "Dame el tercer numero: ";
-    std::cin >> num3;
+    cout << "Dame el primer numero: ";
+    cin >> num1;
+    cout << "Dame el segundo numero: ";
+    cin >> num2;
+    cout << "Dame el tercer numero: ";
+    cin >> num3;
 
     if (num1 == 1 && num2 == 2 && num3 == 3) {
-        std::cout << "Acceso permitido\n";
+        cout << "Acceso permitido\n";
     } else {
-        std::cout << "Acceso rechazado\n";
+        cout << "Acceso rechazado\n";
     }
 
     return 0;
