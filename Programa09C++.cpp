4949635950c6e4f5ae8404304fd157f1a2aeacdf
Luisa Lima <168607491+luilimon@users.noreply.github.com>
2024-06-06 12:37:06 -0600

    Create Programa09C++.cpp

---
+++ Programa09C++.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"ingrese la cantidad a la que quiera llegar";
+    cin>>n;
+    for(int i=0; i<=n ;i+=5)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
